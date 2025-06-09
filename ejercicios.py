dc3a9d59434acfdb8a6ff1df0feb22d149935653
Patricio Romo <patricio.romo99@gmail.com>
2025-06-09 19:55:43 -0400

    incorporacion_RestaOkaPoke
    Se agrega la resta a la calculadora.

--- ejercicios.py
+++ ejercicios.py
@@ -2,3 +2,9 @@ calculo1=int(input("INgrese valor 1"))
 calculo2=int(input("INgrese valor 2"))
 
 print("La suma es ",calculo1+calculo2)
+
+calculo1=int(input("INgrese valor 1"))
+calculo2=int(input("INgrese valor 2"))
+
+print("La resta es ",calculo1-calculo2)
+
